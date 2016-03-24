9a40cafe1cc345bfcea6811386097c98dd973fed
Mark eM <Monter@users.noreply.github.com>
2016-03-24 15:01:56 +0100

    Adding simple local cache

--- proxy.php
+++ proxy.php
@@ -11,9 +11,25 @@
  * @license  GPL http://www.gnu.org/licenses/gpl.html
  * @link     https://rudloff.pro/
  */
+$path =  realpath(dirname(__FILE__));
+$cacheavatarpath = str_replace("plugins", "cache", $path);
+if(!file_exists( $cacheavatarpath )) {
+  $oldmask = umask(0);
+  mkdir($cacheavatarpath, 0777, true);
+  umask($oldmask);
+}
+$cacheavatarlink = $cacheavatarpath . "/" . $_GET['query'];
+if(!file_exists($cacheavatarlink)) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'https://secure.gravatar.com/avatar/'.$_GET['query']);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $out=curl_exec($ch);
+  $fp = fopen($cacheavatarlink, 'w');
+  fwrite($fp, $out);
+  fclose($fp);
+  curl_close($ch);
+}
 header('Content-type: image/png');
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://secure.gravatar.com/avatar/'.$_GET['query']);
-curl_exec($ch);
-curl_close($ch);
+readfile($cacheavatarlink);
 ?>
