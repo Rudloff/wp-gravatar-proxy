693d7be226cf1794be109e5c9af692793119ad8d
Mark eM <Monter@users.noreply.github.com>
2016-03-25 10:46:09 +0100

    PSR-2 indenting

--- proxy.php
+++ proxy.php
@@ -14,21 +14,21 @@
 $path =  realpath(dirname(__FILE__));
 $cacheavatarpath = str_replace("plugins", "cache", $path);
 if(!file_exists( $cacheavatarpath )) {
-  $oldmask = umask(0);
-  mkdir($cacheavatarpath, 0777, true);
-  umask($oldmask);
+    $oldmask = umask(0);
+    mkdir($cacheavatarpath, 0777, true);
+    umask($oldmask);
 }
 $cacheavatarlink = $cacheavatarpath . "/" . $_GET['query'];
 if(!file_exists($cacheavatarlink)) {
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, 'https://secure.gravatar.com/avatar/'.$_GET['query']);
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $out=curl_exec($ch);
-  $fp = fopen($cacheavatarlink, 'w');
-  fwrite($fp, $out);
-  fclose($fp);
-  curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://secure.gravatar.com/avatar/'.$_GET['query']);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $out=curl_exec($ch);
+    $fp = fopen($cacheavatarlink, 'w');
+    fwrite($fp, $out);
+    fclose($fp);
+    curl_close($ch);
 }
 header('Content-type: image/png');
 readfile($cacheavatarlink);
